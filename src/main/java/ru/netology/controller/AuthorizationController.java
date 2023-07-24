fe35aa9b2cb0c951b97397ebc125f82f43874727
Garrik Ten <ten@otux.ru>
2023-07-24 17:03:28 +0300

    edited AuthorizationController.java

--- src/main/java/ru/netology/controller/AuthorizationController.java
+++ src/main/java/ru/netology/controller/AuthorizationController.java
@@ -12,15 +12,17 @@ import java.util.List;
 
 import ru.netology.service.AuthorizationService;
 import ru.netology.authority.Authorities;
-import ru.netology.model.Account;
 
 @RestController
 public class AuthorizationController {
     AuthorizationService service;
+
+	AuthorizationController(AuthorizationService service) {
+		this.service = service;
+	}
     
     @GetMapping("/authorize")
-    public List<Authorities> getAuthorities(@Valid Account account) {
-        return service.getAuthorities(account);
+    public List<Authorities> getAuthorities(@RequestParam("user") String user, @RequestParam("password") String password) {
+        return service.getAuthorities(user, password);
     }
 }
-
